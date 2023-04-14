fdfeed8e2f66367fc639216d48dc7a3d3f58b382
diegocgayoso <diegoc.gayoso@gmail.com>
2023-04-14 11:31:25 -0300

    aprendizados de pipes customizados, melhoria de fluxo de solicitação com uso do switchMap, filter e debounce, e uso do pipe async no template

--- src/app/views/lista-livros/lista-livros.component.ts
+++ src/app/views/lista-livros/lista-livros.component.ts
@@ -1,53 +1,38 @@
-import { Component, OnDestroy } from '@angular/core';
-import { Subscription } from 'rxjs';
-import { Livro } from 'src/app/models/interfaces';
+import { Component } from '@angular/core';
+import { FormControl } from '@angular/forms';
+import { debounceTime, distinctUntilChanged, filter, map, switchMap, tap } from 'rxjs';
+import { Item } from 'src/app/models/interfaces';
+import { LivroVolumeInfo } from 'src/app/models/livroVolumeInfo';
 import { LivroService } from 'src/app/service/livro.service';
 
+const PAUSA = 500;
 @Component({
   selector: 'app-lista-livros',
   templateUrl: './lista-livros.component.html',
   styleUrls: ['./lista-livros.component.css']
 })
-export class ListaLivrosComponent implements OnDestroy {
+export class ListaLivrosComponent {
 
-  listaLivros: Livro[];
-  campoBusca = '';
-  subcription: Subscription;
-  livro: Livro;
+  campoBusca = new FormControl();
 
   constructor(private livroService: LivroService) { }
 
-  buscarLivro() {
-    this.subcription = this.livroService.buscar(this.campoBusca)
-      .subscribe({
-        next: (items) => {
-          this.listaLivros = this.livrosResultadoParaLivros(items)
-        },
-        error: erro => console.error(erro)
-      }
-      );
-  }
+  livrosEncontrados$ = this.campoBusca.valueChanges
+    .pipe(
+      debounceTime(PAUSA),
+      filter((valorDigitado) => valorDigitado.length >= 3),
+      tap(() => console.log('Fluxo inicial')),
+      distinctUntilChanged(),
+      switchMap((valorDigitado) => this.livroService.buscar(valorDigitado)),
+      tap((retornoAPI) => console.log(retornoAPI)),
+      map((items) => this.livrosResultadoParaLivros(items))
+    )
 
-  livrosResultadoParaLivros(items) {
-    const livros: Livro[] = []
-    items.forEach(item => {
-      livros.push(this.livro = {
-        title: item.volumeInfo?.title,
-        authors: item.volumeInfo?.authors,
-        publisher: item.volumeInfo?.publisher,
-        publishedDate: item.volumeInfo?.publishedDate,
-        description: item.volumeInfo?.description,
-        previewLink: item.volumeInfo?.previewLink,
-        thumbnail: item.volumeInfo?.imageLinks?.thumbnail
-      })
+  livrosResultadoParaLivros(items: Item[]): LivroVolumeInfo[] {
+    return items.map(item => {
+      return new LivroVolumeInfo(item);
     })
-    return livros;
   }
-
-  ngOnDestroy(): void {
-    this.subcription.unsubscribe();
-  }
-
 }
 
 
