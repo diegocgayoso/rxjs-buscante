ad2f6f9a34b3a30806b0e841d55992fb8131d80a
diegocgayoso <diegoc.gayoso@gmail.com>
2023-04-17 14:53:58 -0300

    Criando mensagem de erro no lado do cliente e do dev.

--- src/app/service/livro.service.ts
+++ src/app/service/livro.service.ts
@@ -11,14 +11,14 @@ export class LivroService {
   private readonly API_BOOKS_GOOGLE = 'https://www.googleapis.com/books/v1/volumes'
   constructor(private http: HttpClient) { }
 
-  buscar(valorDigitado: string): Observable<Item[]> {
+  buscar(valorDigitado: string): Observable<LivrosResultado> {
     const params = new HttpParams().append('q', valorDigitado);
     return this.http
       .get<LivrosResultado>(this.API_BOOKS_GOOGLE, { params })
-      .pipe(
-        tap(retornoAPI => console.log('Fluxo do tap', retornoAPI)),
-        map(resultado => resultado.items),
-        tap(resultado => console.log('Fluxo após o map', resultado))
-      )
+      // .pipe(
+        // tap(retornoAPI => console.log('Fluxo do tap', retornoAPI)),
+        // map(resultado => resultado.items ?? []),
+        // tap(resultado => console.log('Fluxo após o map', resultado))
+      // )
   }
 }
